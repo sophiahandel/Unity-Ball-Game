b6181066e1c82cbf86f719bf3ddffb638477400f
Sophie Handel <sophiehandel@lawn-143-215-92-215.lawn.gatech.edu>
2019-08-27 12:54:39 -0400

    A simple game in Unity created for Video Game Design class

---
+++ Assets/Scripts/Bobber.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+
+public class Bobber : MonoBehaviour {
+
+    public float amplitude = 0.5f;
+    public float frequency = 1f;
+ 
+    Vector3 posOffset = new Vector3 ();
+    Vector3 tempPos = new Vector3 ();
+ 
+    void Start () {
+        posOffset = transform.position;
+    }
+     
+    // Update is called once per frame
+    void Update () {
+
+        // Float up/down with a Sin()
+        tempPos = posOffset;
+        tempPos.y += Mathf.Sin (Time.fixedTime * Mathf.PI * frequency) * amplitude;
+ 
+        transform.position = tempPos;
+    }
+}
